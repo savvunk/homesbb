5b599cfb9f26eb69c740cf625bd476df986c1745
Silaev Dmitry <dmsilaev@yandex.ru>
2013-04-07 17:14:08 +0400

    New commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homespb::Application.config.secret_token = '6bb0edda56566ca960872bfd086e0f1fcdbd7c0314dd9de6bd79798e089b580ea8f644d346d0f8c6a4598d77d41780adb3262335cffd76da64e618e4f246358f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Homespb::Application.config.session_store :cookie_store, key: '_homespb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Homespb::Application.config.session_store :active_record_store
